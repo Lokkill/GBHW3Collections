c398b10869422ba6abe2a3d2e976463a30e65002
Андрей Катаев <crovolk@yandex.ru>
2020-11-09 11:06:46 +0300

    Completed home work #3

--- src/our/company/Main.java
+++ src/our/company/Main.java
@@ -1,7 +1,50 @@
 package our.company;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        /*1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов,
+        из которых состоит массив (дубликаты не считаем). Посчитать сколько раз встречается каждое слово.*/
+        String[] listWords = {"after", "before", "apple", "banana", "after", "close", "red", "string", "word", "after", "apple"};
+        getListInfo(listWords);
+
+
+        /* 2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
+        В этот телефонный справочник с помощью метода add() можно добавлять записи. С помощью метода get() искать номер телефона по фамилии.
+        Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+        тогда при запросе такой фамилии должны выводиться все телефоны.*/
+        TelephoneDirectory td = new TelephoneDirectory();
+        td.adds("Petrov", "+79996541232");
+        td.adds("Mishkov", "+79835647885");
+        td.adds("Krutov", "+79652369855");
+        td.adds("Petrov", "+79893571596");
+        td.get("Petrov");
+        td.get("Mishkov");
+        td.get("Krutov");
+    }
+
+    public static void getListInfo(String[] arr){
+
+        Map<String, Integer> mapArr = new HashMap<>();
+        for (int i = 0; i < arr.length; i++){
+            if (mapArr.containsKey(arr[i])){
+                int count = mapArr.get(arr[i]);
+                count++;
+                mapArr.put(arr[i], count);
+            } else {
+                mapArr.put(arr[i], 1);
+            }
+        }
+        for (Map.Entry<String, Integer> o : mapArr.entrySet()){
+            System.out.println(MessageFormat.format("Уникальное значение: {0}", o.getKey()));
+        }
+        System.out.println();
+        for (Map.Entry<String, Integer> o : mapArr.entrySet()){
+            System.out.println(MessageFormat.format("Значение: {0}, Количество: {1}", o.getKey(), o.getValue()));
+        }
 
     }
 }
--- src/our/company/TelephoneDirectory.java
+++ src/our/company/TelephoneDirectory.java
@@ -1,4 +1,33 @@
 package our.company;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TelephoneDirectory {
+    private Map<String, ArrayList<String>> maps = new HashMap<>();
+
+    public TelephoneDirectory() {
+    }
+
+    public void adds(String secondName, String telephoneNumber){
+        if (maps.containsKey(secondName)){
+            ArrayList<String> list = maps.get(secondName);
+            list.add(telephoneNumber);
+            maps.put(secondName, list);
+        } else {
+            ArrayList<String> listPhoneNumber = new ArrayList<>();
+            listPhoneNumber.add(telephoneNumber);
+            maps.put(secondName, listPhoneNumber);
+        }
+    }
+
+    public void get(String secondName){
+        System.out.println(MessageFormat.format("Номера телефона контакта {0}: ", secondName));
+        for (String number : maps.get(secondName)){
+            System.out.println("\t" + number);
+        }
+        System.out.println();
+    }
 }
